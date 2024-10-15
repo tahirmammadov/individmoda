ac9853da06c4ca73203e3ff9b6d66dd76b86a21f
Tahir <tmammadovt@gmail.com>
2024-10-15 17:48:29 +0400

    card is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,107 @@
 function App() {
   return (
      <div className="wrapper">
+
       <header>
-        <div className="header-left">
-          <img width={50} height={50} src="/img/logo.png" alt="Logo" />
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" alt="Logo" />
           <div>
             <h3>Individmoda</h3>
             <p>Women's Clothing & Fashion</p>
           </div>
         </div>
-        <ul className="header-right">
-          <li>
-            <img width={15} height={15} src="/img/cart_icon.svg" alt="Cart" />
+        <ul className="headerRight">
+          <li className="li1">
+            <img width={18} height={18} src="/img/cart_icon.svg" alt="Cart" />
             <span>1205 azn</span>
           </li>
           <li>
-            <img width={15} height={15} src="/img/user_icon.svg" alt="" />
+            <img width={18} height={18} src="/img/user_icon.svg" alt="User" />
           </li>
         </ul>
       </header>
+
+
+
       <div className="content">
-        <h1>All clothes</h1>
-        ...
+        <div className="headingAndSearch">
+          <h1>All clothes</h1>
+          <div className="searchBar">
+            <img width={18} height={18} src="./img/search_icon.svg" alt="Search" />
+            <input placeholder="Search..."></input>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <button className="heart_icon_dev">
+              <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
+            </button>      
+            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/1.jpeg" alt="ProductPicture" />
+            <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
+            <div className="priceAndPlus">
+              <div className="price">
+                <span>Price</span>
+                <b>85 azn</b>
+              </div>
+              <button className="plus_icon_dev">
+                <img width={11} height={11} src="/img/plus_icon.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <button className="heart_icon_dev">
+              <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
+            </button>      
+            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/2.jpeg" alt="ProductPicture" />
+            <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
+            <div className="priceAndPlus">
+              <div className="price">
+                <span>Price</span>
+                <b>85 azn</b>
+              </div>
+              <button className="plus_icon_dev">
+                <img width={11} height={11} src="/img/plus_icon.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <button className="heart_icon_dev">
+              <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
+            </button>      
+            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/3.jpeg" alt="ProductPicture" />
+            <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
+            <div className="priceAndPlus">
+              <div className="price">
+                <span>Price</span>
+                <b>85 azn</b>
+              </div>
+              <button className="plus_icon_dev">
+                <img width={11} height={11} src="/img/plus_icon.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <button className="heart_icon_dev">
+              <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
+            </button>     
+            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/4.jpeg" alt="ProductPicture" />
+            <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
+            <div className="priceAndPlus">
+              <div className="price">
+                <span>Price</span>
+                <b>85 azn</b>
+              </div>
+              <button className="plus_icon_dev">
+                <img width={11} height={11} src="/img/plus_icon.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+      
+
+          
+        </div>
+
       </div>
      </div>
   )
