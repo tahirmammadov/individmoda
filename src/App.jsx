1542134dd37e79fad893040aea825034f5aba5a6
Tahir <tmammadovt@gmail.com>
2024-10-18 17:49:34 +0400

    cart almost finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,77 @@
 function App() {
   return (
-     <div className="wrapper">
+    <div className="wrapper">
+
+      <div className="overlay">
+        <div className="drawer">
+          <div className="cartHeadingAndClose">
+            <h1>Cart</h1>
+            <button className="cartCloseButton">
+              <img width={18} height={18} src="/img/close_icon.svg" alt="CloseButton" />
+            </button>
+          </div>
+          <div className="cartItems">
+            <div className="cartItem">
+              <img className="cartItemImage" width={100} height={133} src="/img/clothing/1.jpeg" alt="CartProduct" />
+              <div className= "description">
+                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum quo, numquam</h5>
+                <b>85 azn</b>
+              </div>
+              <button className="cartItemCloseButton">
+                <img width={15} height={15} src="/img/close_icon.svg" alt="CloseButton" />
+              </button>
+            </div>
+            <div className="cartItem">
+              <img className="cartItemImage" width={100} height={133} src="/img/clothing/2.jpeg" alt="CartProduct" />
+              <div className= "description">
+                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum quo, numquam</h5>
+                <b>85 azn</b>
+              </div>
+              <button className="cartItemCloseButton">
+                <img width={15} height={15} src="/img/close_icon.svg" alt="CloseButton" />
+              </button>
+            </div>
+            <div className="cartItem">
+              <img className="cartItemImage" width={100} height={133} src="/img/clothing/3.jpeg" alt="CartProduct" />
+              <div className= "description">
+                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum quo, numquam</h5>
+                <b>85 azn</b>
+              </div>
+              <button className="cartItemCloseButton">
+                <img width={15} height={15} src="/img/close_icon.svg" alt="CloseButton" />
+              </button>
+            </div>
+            <div className="cartItem">
+              <img className="cartItemImage" width={100} height={133} src="/img/clothing/4.jpeg" alt="CartProduct" />
+              <div className= "description">
+                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum quo, numquam</h5>
+                <b>85 azn</b>
+              </div>
+              <button className="cartItemCloseButton">
+                <img width={15} height={15} src="/img/close_icon.svg" alt="CloseButton" />
+              </button>
+            </div>
+          </div>
+          <div className="cartTotals">
+            <ul>
+              <li>
+                <span>Total: </span>
+                <div></div>
+                <b>1250 azn</b>
+              </li>
+              <li>
+                <span>Tax 5%: </span>
+                <div></div>
+                <b>62.5 azn</b>
+              </li>
+            </ul>
+            <button className="checkout">
+              <div>Checkout</div>
+              <img width={18} height={18}  src="/img/arrow_right_icon.svg" alt="Arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
 
       <header>
         <div className="headerLeft">
@@ -36,8 +107,8 @@ function App() {
           <div className="card">
             <button className="heart_icon_dev">
               <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
-            </button>      
-            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/1.jpeg" alt="ProductPicture" />
+            </button>
+            <img width={240} height={240 / (1200 / 1600)} className="product" src="/img/clothing/1.jpeg" alt="ProductPicture" />
             <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
             <div className="priceAndPlus">
               <div className="price">
@@ -52,8 +123,8 @@ function App() {
           <div className="card">
             <button className="heart_icon_dev">
               <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
-            </button>      
-            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/2.jpeg" alt="ProductPicture" />
+            </button>
+            <img width={240} height={240 / (1200 / 1600)} className="product" src="/img/clothing/2.jpeg" alt="ProductPicture" />
             <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
             <div className="priceAndPlus">
               <div className="price">
@@ -68,8 +139,8 @@ function App() {
           <div className="card">
             <button className="heart_icon_dev">
               <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
-            </button>      
-            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/3.jpeg" alt="ProductPicture" />
+            </button>
+            <img width={240} height={240 / (1200 / 1600)} className="product" src="/img/clothing/3.jpeg" alt="ProductPicture" />
             <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
             <div className="priceAndPlus">
               <div className="price">
@@ -84,8 +155,8 @@ function App() {
           <div className="card">
             <button className="heart_icon_dev">
               <img width={15} height={15} src="/img/heart_unliked_icon.svg" alt="Heart" />
-            </button>     
-            <img width={240} height={240/(1200/1600)} className="product" src="/img/clothing/4.jpeg" alt="ProductPicture" />
+            </button>
+            <img width={240} height={240 / (1200 / 1600)} className="product" src="/img/clothing/4.jpeg" alt="ProductPicture" />
             <h5 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo accusantium architecto suscipit</h5>
             <div className="priceAndPlus">
               <div className="price">
@@ -97,13 +168,13 @@ function App() {
               </button>
             </div>
           </div>
-      
 
-          
+
+
         </div>
 
       </div>
-     </div>
+    </div>
   )
 }
 
